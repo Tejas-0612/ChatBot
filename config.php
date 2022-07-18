e2833155df0aff981c2d0c4a6ab0b7a798670e6c
Tejas-0612 <91648258+Tejas-0612@users.noreply.github.com>
2022-07-18 22:52:24 +0530

    initial commit

---
+++ config.php
@@ -0,0 +1,14 @@
+<!-- <?php 
+
+$server = "localhost";
+$user = "root";
+$pass = "";
+$database = "cb2";
+
+$conn = mysqli_connect($server, $user, $pass, $database,3306);
+
+if (!$conn) {
+    die("<script>alert('Connection Failed.')</script>");
+}
+
+?> -->
